a17905b2ba1947ec751b8a06e5941b259559b87d
achen116 <chen.arielle@gmail.com>
2015-08-10 19:20:47 -0700

    create rails skeleton, migrations, controller, and initial seed file

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,7 @@
+class OrganizationsController < ApplicationController
+
+	def index
+		@organizations = Organization.all
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root 'organizations#index'
+end
