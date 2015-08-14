ad9f2432de999a91a9e4395f4dadb0995f5933ee
achen116 <chen.arielle@gmail.com>
2015-08-13 22:44:48 -0700

    add static page controller for about and contact routes

---
+++ app/helpers/beyondmiles_helper.rb
@@ -0,0 +1,2 @@
+module BeyondmilesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'organizations#index'
 
   resources :organizations, only: [:index, :show]
+  
+  get 'beyondmiles/about'
+  get 'beyondmiles/contact'
 end
